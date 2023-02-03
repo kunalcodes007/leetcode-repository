0161426f7176adebe11ca1b80cab2428caad81b1
kunal Lingwal <kunallingwal2@gmail.com>
2023-02-03 15:35:11 +0530

    Added solution - LeetHub

---
+++ Armstrong Numbers - GFG/armstrong-numbers.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string armstrongNumber(int n){
+        
+    int cube=0;
+        
+        int temp=n;
+        while(temp!=0){
+            
+            int digit=temp % 10;
+            cube=(cube)+(digit*digit*digit);
+            temp/=10;
+            
+            
+        }
+        
+        if(cube==n){
+            return ("Yes");}
+            else{
+                return ("No");
+        }
+        
+        
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
